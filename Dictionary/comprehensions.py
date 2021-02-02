24fe3bde52501779a6d3018a1f71efaf7ae17ec9
Nataraj.R <raj13aug@gmail.com>
2021-02-02 21:49:59 +0530

    data

---
+++ Dictionary/comprehensions.py
@@ -0,0 +1,7 @@
+# Syntax
+
+#dict = {key:value for vars in iterable}
+
+dicts = {x: x*2 for x in range(1, 20)}
+
+print(dicts)
---
+++ Dictionary/data_type.py
@@ -0,0 +1,11 @@
+num = 7
+amt = 123.45
+code = 'A'
+pi = 3.141
+population_of_india = 100000000
+msg = "Hi"
+
+print(" Num =" + str(num))
+print("\nAMT =" + str(amt))
+print("CODE =" + str(code))
+print("Msg = " + msg)
